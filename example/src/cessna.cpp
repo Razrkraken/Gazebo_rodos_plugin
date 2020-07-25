25ee0b5be169c3589072e13b94a49a9624cca780
Abestanis <abestanis.gc@gmail.com>
2020-07-25 16:10:10 +0200

    Document the example code

--- example/src/cessna.cpp
+++ example/src/cessna.cpp
@@ -1,4 +1,3 @@
-
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/transport.hh>
@@ -6,21 +5,22 @@
 
 
 namespace gazebo {
-    /// \brief A plugin to control the Cessna.
+    /**
+     * A plugin to control the Cessna via gazebo topics.
+     */
     class CessnaPlugin : public ModelPlugin {
-        /// \brief Constructor
     public:
         CessnaPlugin() {}
 
-        /// \brief The load function is called by Gazebo when the plugin is
-        /// inserted into simulation
-        /// \param[in] _model A pointer to the model that this plugin is
-        /// attached to.
-        /// \param[in] _sdf A pointer to the plugin's SDF element.
-        virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
-
+        /***
+         * The load function is called by Gazebo when the plugin is inserted into simulation
+         *
+         * @param model A pointer to the model that this plugin is attached to.
+         * @param sdf A pointer to the plugin's SDF element.
+         */
+        virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf) {
             // Store the model pointer for convenience.
-            this->model = _model;
+            this->model = model;
 
             std::array<std::string, 6> JOINT_NAMES = {
                     "elevators_joint",
@@ -30,9 +30,8 @@ namespace gazebo {
                     "right_flap_joint",
                     "right_aileron_joint",
             };
-
             for (auto name : JOINT_NAMES) {
-                physics::JointPtr joint = _model->GetJoint("cessna_c172::" + name);
+                physics::JointPtr joint = model->GetJoint("cessna_c172::" + name);
                 if (joint == nullptr) {
                     gzerr << "Missing " << name << " joint\n";
                     return;
@@ -52,48 +51,67 @@ namespace gazebo {
             this->node->Init(this->model->GetWorld()->Name());
 #endif
 
-            // Create a topic name
-            std::string topicName1 = "~/" + this->model->GetName() + "/elevator_angle";
-            std::string topicName2 = "~/" + this->model->GetName() + "/rudder_angle";
+            // Create the topic names
+            std::string elevatorTopicName = "~/" + this->model->GetName() + "/elevator_angle";
+            std::string rudderTopicName = "~/" + this->model->GetName() + "/rudder_angle";
 
 
-            // Subscribe to the topic, and register a callback
-            this->sub1 = this->node->Subscribe(topicName1,
-                                               &CessnaPlugin::OnMsgElevator, this);
-            this->sub2 = this->node->Subscribe(topicName2,
-                                               &CessnaPlugin::OnMsgRudder, this);
+            // Subscribe to the topic and register a callback
+            this->elevatorSubscriber = this->node->Subscribe(
+                    elevatorTopicName, &CessnaPlugin::OnMsgElevator, this);
+            this->rudderSubscriber = this->node->Subscribe(
+                    rudderTopicName, &CessnaPlugin::OnMsgRudder, this);
 
 
-            //Giving initial Velocity
-            if (_sdf->HasElement("initial_vel")) {
-                math::Vector3 initial_vel = _sdf->GetElement("initial_vel")->Get<math::Vector3>();
-                _model->GetLink("cessna_c172::body")->SetLinearVel(initial_vel);
+            // Set the initial velocity of the cessna
+            if (sdf->HasElement("initial_vel")) {
+                math::Vector3 initial_vel = sdf->GetElement("initial_vel")->Get<math::Vector3>();
+                model->GetLink("cessna_c172::body")->SetLinearVel(initial_vel);
             }
 
         }
 
     private:
-        /// \brief Handle incoming message
-        /// \param[in] _msg Repurpose a vector2 message. This function will
-        /// only use the x component.
-        void OnMsgElevator(ConstVector2dPtr &_msg) {
-            this->model->GetJointController()->SetPositionTarget(joints["elevators_joint"]->GetScopedName(), _msg->x());
+        /**
+         * Set the target value of the elevator PID controller.
+         * 
+         * @param msg A vector containing the target value as the x component.
+         */
+        void OnMsgElevator(ConstVector2dPtr &msg) {
+            this->model->GetJointController()->SetPositionTarget(joints["elevators_joint"]->GetScopedName(), msg->x());
         }
-
+    
+        /**
+         * Set the target value of the rudder PID controller.
+         * 
+         * @param msg A vector containing the target value as the x component.
+         */
         void OnMsgRudder(ConstVector2dPtr &_msg) {
             this->model->GetJointController()->SetPositionTarget(joints["rudder_joint"]->GetScopedName(), _msg->x());
         }
 
-        /// \brief Pointer to the model.
+        /**
+         * Pointer to the model.
+         */
         physics::ModelPtr model;
 
-        /// \brief A node used for transport
+        /**
+         * A node used for transport
+         */
         transport::NodePtr node;
 
-        /// \brief A subscriber to a named topic.
-        transport::SubscriberPtr sub1;
-        transport::SubscriberPtr sub2;
+        /**
+         * A subscriber to the Gazebo elevator topic.
+         */
+        transport::SubscriberPtr elevatorSubscriber;
+        /**
+         * A subscriber to the Gazebo rudder topic.
+         */
+        transport::SubscriberPtr rudderSubscriber;
 
+        /**
+         * The joints of the cessna by name.
+         */
         std::map<std::string, physics::JointPtr> joints;
     };
 
